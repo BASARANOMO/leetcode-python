84c7bf148a450a60f8c10258cc47c218cdac471b
BASARANOMO <39518094+BASARANOMO@users.noreply.github.com>
2021-02-05 09:11:02 +0100

    add solution

--- solutions/Medium/63. Unique Paths II/solution.py
+++ solutions/Medium/63. Unique Paths II/solution.py
@@ -41,3 +41,18 @@ class Solution:
                 else:
                     obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
         return obstacleGrid[-1][-1]
+
+    def uniquePathsWithObstacles_opt2(self, obstacleGrid: List[List[int]]) -> int:
+        if not obstacleGrid or obstacleGrid[0][0] == 1:
+            return 0
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+
+        obstacleGrid[0][0] = 1 - obstacleGrid[0][0]
+        for i in range(1, m):
+            obstacleGrid[i][0] = (1 - obstacleGrid[i][0]) * obstacleGrid[i-1][0]
+        for j in range(1, n):
+            obstacleGrid[0][j] = (1 - obstacleGrid[0][j]) * obstacleGrid[0][j-1]
+        for i in range(1, m):
+            for j in range(1, n):
+                obstacleGrid[i][j] = (obstacleGrid[i-1][j] + obstacleGrid[i][j-1]) * (1 - obstacleGrid[i][j])
+        return obstacleGrid[-1][-1]
