50f2529ea66e605ade44aa169d55dbc457fae0fb
BASARANOMO <39518094+BASARANOMO@users.noreply.github.com>
2021-02-05 07:55:19 +0100

    update

--- solutions/Medium/62. Unique Paths/solution.py
+++ solutions/Medium/62. Unique Paths/solution.py
@@ -12,3 +12,22 @@ class Solution:
                 return self.factorial(val-1, nums-1, ascendent) * val
 
         return 1
+
+    # DP
+    def uniquePaths_DP(self, m: int, n: int) -> int:
+        if (m == 0) or (n == 0):
+            return 0
+        dp_matrix = [[1 for _ in range(n)] for _ in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp_matrix[i][j] = dp_matrix[i-1][j] + dp_matrix[i][j-1]
+        return dp_matrix[-1][-1]
+
+    def uniquePaths_DP_opt(self, m: int, n: int) -> int:
+        if (m == 0) or (n == 0):
+            return 0
+        dp_matrix = [1 for _ in range(n)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp_matrix[j] = dp_matrix[j] + dp_matrix[j-1]
+        return dp_matrix[-1]
