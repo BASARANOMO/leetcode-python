0eaca35c8d067d70d263b52a3add83b1207b64d8
BASARANOMO <39518094+BASARANOMO@users.noreply.github.com>
2021-02-07 18:10:10 +0100

    update

---
+++ solutions/Easy/1114. Print in Order/solution.py
@@ -0,0 +1,21 @@
+from threading import Lock
+
+class Foo:
+    def __init__(self):
+        self.locks = (Lock(),Lock())
+        self.locks[0].acquire()
+        self.locks[1].acquire()
+
+    def first(self, printFirst):
+        printFirst()
+        self.locks[0].release()
+
+    def second(self, printSecond):
+        with self.locks[0]:
+            printSecond()
+            self.locks[1].release()
+
+
+    def third(self, printThird):
+        with self.locks[1]:
+            printThird()
