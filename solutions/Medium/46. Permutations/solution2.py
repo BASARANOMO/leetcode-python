16f10a091ddd1bb7a6fa6210563105f7d61e074f
BASARANOMO <39518094+BASARANOMO@users.noreply.github.com>
2021-02-07 10:23:45 +0100

    add solutions

---
+++ solutions/Medium/46. Permutations/solution2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+        visited = [0] * n
+        self.backtracking(n, nums, visited, 0, [], res)
+        return res
+
+
+    def backtracking(self, n, nums, visited, pos, curr, res):
+        if (len(curr) == n):
+            res.append(curr.copy())
+        for i in range(0, n):
+            if (visited[i] == 0):
+                curr.append(nums[i])
+                visited[i] = 1
+                self.backtracking(n, nums, visited, pos+1, curr, res)
+                curr.pop()
+                visited[i] = 0
---
+++ solutions/Medium/46. Permutations/solution3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.backtracking(nums, [], res)
+        return res
+
+    def backtracking(self, nums, curr, res):
+        if not nums:
+            res.append(curr)
+        for i in range(len(nums)):
+            self.backtracking(nums[:i]+nums[i+1:], curr+[nums[i]], res)
