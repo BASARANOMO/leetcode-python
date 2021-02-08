cba709252dc4dbb036cf3fe6bbdae1097891076d
BASARANOMO <39518094+BASARANOMO@users.noreply.github.com>
2021-02-08 08:55:23 +0100

    update

--- solutions/Medium/47. Permutations II/solution.py
+++ solutions/Medium/47. Permutations II/solution.py
@@ -9,3 +9,25 @@ class Solution:
             res.append(curr)
         for i in range(len(nums)):
             self.backtracking(nums[:i]+nums[i+1:], curr+[nums[i]], res)
+
+    def permuteUnique2(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        num_groups = {}
+        for num in nums:
+            if not num_groups.get(num):
+                num_groups[num] = 1
+            else:
+                num_groups[num] += 1
+        self.backtracking(len(nums), num_groups, [], res)
+        return res
+
+    def backtracking(self, n, num_groups, curr, res):
+        if len(curr) == n:
+            res.append(curr[:])
+        for num in num_groups:
+            if num_groups[num] > 0:
+                num_groups[num] -= 1
+                curr.append(num)
+                self.backtracking(n, num_groups, curr, res)
+                num_groups[num] += 1
+                curr.pop()
