aa090bb378e0705c737e0e1b2927b59ce58952aa
BASARANOMO <39518094+BASARANOMO@users.noreply.github.com>
2021-02-08 06:51:39 +0100

    update

---
+++ solutions/Easy/1688. Count of Matches in Tournament/README.md
@@ -0,0 +1,13 @@
+# 1688. Count of Matches in Tournament
+
+[LeetCode Page](https://leetcode.com/problems/count-of-matches-in-tournament/)
+
+## Solutions
+### Naive solution
+return n-1
+
+### Stupid solution
+```
+even: increment n / 2
+odd: increment (n - 1) / 2 + 1
+```
---
+++ solutions/Easy/1688. Count of Matches in Tournament/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        res = 0
+        while n != 1:
+            if n % 2 == 0: # even
+                res += n / 2
+                n /= 2
+            else: # odd
+                res += (n - 1) / 2
+                n = (n - 1) / 2 + 1
+        return int(res)
+
+    def numberOfMatches_naive(self, n: int) -> int:
+        return n-1
