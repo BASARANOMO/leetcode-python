9c350e4ee079e2bcb7c2ea1663c0b720a41be1b8
BASARANOMO <basaranomo@outlook.com>
2022-07-15 21:06:10 +0800

    black formatted

--- solutions/Medium/558. Logical OR of Two Binary Grids Represented as Quad-Trees/solution.py
+++ solutions/Medium/558. Logical OR of Two Binary Grids Represented as Quad-Trees/solution.py
@@ -10,8 +10,9 @@ class Node:
         self.bottomRight = bottomRight
 """
 
+
 class Solution:
-    def intersect(self, quadTree1: 'Node', quadTree2: 'Node') -> 'Node':
+    def intersect(self, quadTree1: "Node", quadTree2: "Node") -> "Node":
         if quadTree1.isLeaf:
             return Node(True, True) if quadTree1.val else quadTree2
         if quadTree2.isLeaf:
@@ -20,6 +21,12 @@ class Solution:
         o2 = self.intersect(quadTree1.topRight, quadTree2.topRight)
         o3 = self.intersect(quadTree1.bottomLeft, quadTree2.bottomLeft)
         o4 = self.intersect(quadTree1.bottomRight, quadTree2.bottomRight)
-        if o1.isLeaf and o2.isLeaf and o3.isLeaf and o4.isLeaf and o1.val == o2.val == o3.val == o4.val:
+        if (
+            o1.isLeaf
+            and o2.isLeaf
+            and o3.isLeaf
+            and o4.isLeaf
+            and o1.val == o2.val == o3.val == o4.val
+        ):
             return Node(o1.val, True)
         return Node(False, False, o1, o2, o3, o4)
