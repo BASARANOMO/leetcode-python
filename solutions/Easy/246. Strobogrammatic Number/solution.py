c5e83970b1da8822e667a3276f1ed85d4fb4ec11
BASARANOMO <basaranomo@outlook.com>
2022-08-12 00:20:38 +0800

    black formatting

--- solutions/Easy/246. Strobogrammatic Number/solution.py
+++ solutions/Easy/246. Strobogrammatic Number/solution.py
@@ -1,6 +1,6 @@
 class Solution:
     def isStrobogrammatic(self, num: str) -> bool:
-        corr = {0:0, 1:1, 6:9, 8:8, 9:6}
+        corr = {0: 0, 1: 1, 6: 9, 8: 8, 9: 6}
         l, r = 0, len(num) - 1
         while l <= r:
             num1 = int(num[l])
