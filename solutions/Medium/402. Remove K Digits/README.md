264a48f319719d3f7bc0ebd38e286df38a9a3629
BASARANOMO <basaranomo@outlook.com>
2022-08-14 14:51:07 +0800

    add solution to question 402

---
+++ solutions/Medium/402. Remove K Digits/README.md
@@ -0,0 +1,9 @@
+# 402. Remove K Digits
+
+## Solution
+
+Monotone stack
+
+Time complexity: `O(n)`
+
+Space complexity: `O(n)`
---
+++ solutions/Medium/402. Remove K Digits/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        i, n = 0, len(num)
+        if n <= k:
+            return "0"
+        mntn_stk = []
+        while i < n:
+            val = num[i]
+            while mntn_stk and mntn_stk[-1] > val and k > 0:
+                mntn_stk.pop()
+                k -= 1
+            i += 1
+            mntn_stk.append(val)
+        if k > 0:
+            mntn_stk = mntn_stk[:-k]
+        return "".join(mntn_stk).lstrip('0') or "0"
